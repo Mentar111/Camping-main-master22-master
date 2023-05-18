68fcbf94b1a58696aee0e91d1622d8ade44b630a
Mentar111 <sabratila989@gmail.com>
2023-05-18 13:07:49 +0300

    Camping v1.2.7 + Catalog

--- js/javaScript.js
+++ js/javaScript.js
@@ -54,3 +54,22 @@ function closeOnClick() {
 
 }
 
+/* Когда пользователь нажимает на кнопку,
+переключение между скрытием и отображением раскрывающегося содержимого */
+function myFunction() {
+  document.getElementById("submenu").classList.toggle("show");
+}
+
+// Закройте выпадающее меню, если пользователь щелкает за его пределами
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
